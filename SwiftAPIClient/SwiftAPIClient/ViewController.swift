2a623de67b73309841d799483dfbb23790a4a67a
前田　和樹 <01008969@CA5690.local>
2019-12-28 00:42:11 +0900

    Project Created

---
+++ SwiftAPIClient/SwiftAPIClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftAPIClient
+//
+//  Created by 前田　和樹 on 2019/12/28.
+//  Copyright © 2019 kzk_maeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SwiftAPIClient/SwiftAPIClientTests/SwiftAPIClientTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftAPIClientTests.swift
+//  SwiftAPIClientTests
+//
+//  Created by 前田　和樹 on 2019/12/28.
+//  Copyright © 2019 kzk_maeda. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftAPIClient
+
+class SwiftAPIClientTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
